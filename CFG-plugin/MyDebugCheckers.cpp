152dcddabc333d5c878d3a33b0790bfb7edd4eef
Ronak Chauhan <ronakchauhan97@gmail.com>
2019-01-01 11:43:52 +0530

    Mild refactor

--- CFG-plugin/MyDebugCheckers.cpp
+++ CFG-plugin/MyDebugCheckers.cpp
@@ -41,123 +41,92 @@ namespace {
 class MyCFGDumper : public Checker<check::ASTCodeBody> {
 public:
   void checkASTCodeBody(const Decl *D, AnalysisManager &mgr,
-                        BugReporter &BR) const {
-    llvm::errs() << "----------------\n";
+                        BugReporter &BR) const;
+}; // namespace
 
-    PrintingPolicy Policy(mgr.getLangOpts());
-    Policy.TerseOutput = false;
-    Policy.PolishForDeclaration = true;
-    D->print(llvm::errs(), Policy);
+void MyCFGDumper::checkASTCodeBody(const Decl *D, AnalysisManager &mgr,
+                                   BugReporter &BR) const {
+  llvm::errs() << "----------------\n";
 
-    llvm::errs() << "--------\n";
+  PrintingPolicy Policy(mgr.getLangOpts());
+  Policy.TerseOutput = false;
+  Policy.PolishForDeclaration = true;
+  D->print(llvm::errs(), Policy);
 
-    // STEP 1.1: Print function name
-    llvm::errs() << "FuncName: ";
-    const FunctionDecl *func_decl = dyn_cast<FunctionDecl>(D);
-    llvm::errs() << func_decl->getNameInfo().getAsString() << "\n";
+  llvm::errs() << "--------\n";
 
-    // STEP 1.2: Print function parameters.
-    std::string Proto;
-    llvm::errs() << "Params  : ";
-    if (func_decl
-            ->doesThisDeclarationHaveABody()) { //& !func_decl->hasPrototype())
-                                                //{
-      for (unsigned i = 0, e = func_decl->getNumParams(); i != e; ++i) {
-        if (i)
-          Proto += ", ";
-        const ParmVarDecl *paramVarDecl = func_decl->getParamDecl(i);
-        const VarDecl *varDecl = dyn_cast<VarDecl>(paramVarDecl);
+  // STEP 1.1: Print function name
+  llvm::errs() << "FuncName: ";
+  const FunctionDecl *func_decl = dyn_cast<FunctionDecl>(D);
+  llvm::errs() << func_decl->getNameInfo().getAsString() << "\n";
 
-        // Parameter type
-        QualType T =
-            varDecl->getTypeSourceInfo()
-                ? varDecl->getTypeSourceInfo()->getType()
-                : varDecl->getASTContext().getUnqualifiedObjCPointerType(
-                      varDecl->getType());
-        Proto += T.getAsString();
+  // STEP 1.2: Print function parameters.
+  std::string Proto;
+  llvm::errs() << "Params  : ";
+  if (func_decl
+          ->doesThisDeclarationHaveABody()) { //& !func_decl->hasPrototype())
+                                              //{
+    for (unsigned i = 0, e = func_decl->getNumParams(); i != e; ++i) {
+      if (i)
+        Proto += ", ";
+      const ParmVarDecl *paramVarDecl = func_decl->getParamDecl(i);
+      const VarDecl *varDecl = dyn_cast<VarDecl>(paramVarDecl);
 
-        // Parameter name
-        Proto += " ";
-        Proto += varDecl->getNameAsString();
-      }
+      // Parameter type
+      QualType T = varDecl->getTypeSourceInfo()
+                       ? varDecl->getTypeSourceInfo()->getType()
+                       : varDecl->getASTContext().getUnqualifiedObjCPointerType(
+                             varDecl->getType());
+      Proto += T.getAsString();
+
+      // Parameter name
+      Proto += " ";
+      Proto += varDecl->getNameAsString();
     }
-    llvm::errs() << Proto << "\n";
+  }
+  llvm::errs() << Proto << "\n";
 
-    // STEP 1.3: Print function return type.
-    const QualType returnQType = func_decl->getReturnType();
-    llvm::errs() << "ReturnT : " << returnQType.getAsString() << "\n";
+  // STEP 1.3: Print function return type.
+  const QualType returnQType = func_decl->getReturnType();
+  llvm::errs() << "ReturnT : " << returnQType.getAsString() << "\n";
 
-    // STEP 2: Print the CFG.
-    if (CFG *cfg = mgr.getCFG(D)) {
-      for (auto bb : *cfg) {
-        unsigned bb_id = bb->getBlockID();
-        llvm::errs() << "BB" << bb_id << ":\n";
-        for (auto elem : *bb) {
-          // ref: https://clang.llvm.org/doxygen/CFG_8h_source.html#l00056
-          // ref for printing block:
-          // https://clang.llvm.org/doxygen/CFG_8cpp_source.html#l05234
+  // STEP 2: Print the CFG.
+  if (CFG *cfg = mgr.getCFG(D)) {
+    for (auto bb : *cfg) {
+      unsigned bb_id = bb->getBlockID();
+      llvm::errs() << "BB" << bb_id << ":\n";
+      for (auto elem : *bb) {
+        // ref: https://clang.llvm.org/doxygen/CFG_8h_source.html#l00056
+        // ref for printing block:
+        // https://clang.llvm.org/doxygen/CFG_8cpp_source.html#l05234
 
-          // Dump partial AST for each basic block
-          Optional<CFGStmt> CS = elem.getAs<CFGStmt>();
-          const Stmt *S = CS->getStmt();
-          // S->dump(); // Dumps partial AST
+        // Dump partial AST for each basic block
+        Optional<CFGStmt> CS = elem.getAs<CFGStmt>();
+        const Stmt *S = CS->getStmt();
+        // S->dump(); // Dumps partial AST
 
-          std::string stmt_class = S->getStmtClassName();
+        std::string stmt_class = S->getStmtClassName();
 
-          if (stmt_class == "DeclStmt") {
-            // VarDecl *ident = cast<VarDecl>(S);
-            llvm::errs() << "Variable declaration for \n";
-          }
+        if (stmt_class == "DeclStmt") {
+          // VarDecl *ident = cast<VarDecl>(S);
+          llvm::errs() << "Variable declaration for \n";
+        }
 
-          else if (stmt_class == "DeclRefExpr") {
-            const ValueDecl *ident = cast<DeclRefExpr>(S)->getDecl();
-            llvm::errs() << "DeclRefExpr using " << ident->getName() << "\n";
-          }
+        else if (stmt_class == "DeclRefExpr") {
+          const ValueDecl *ident = cast<DeclRefExpr>(S)->getDecl();
+          llvm::errs() << "DeclRefExpr using " << ident->getName() << "\n";
+        }
 
-          else {
-            llvm::errs() << "found " << stmt_class << "\n";
-          }
+        else {
+          llvm::errs() << "found " << stmt_class << "\n";
         }
       }
     }
   }
-}; // namespace
+}
+
 } // anonymous namespace
 
 void ento::registerMyCFGDumper(CheckerManager &mgr) {
   mgr.registerChecker<MyCFGDumper>();
 }
-
-// if (elem.getKind() == CFGElement::Kind::Statement) {
-//   auto cfgstmt = elem.getAs<CFGStmt>();
-//   //auto stmt = cfgstmt.getStmt();
-//   //stmt->dump();
-//   //llvm::errs() << "AD: " << cfgstmt << "\n";
-// }
-
-// void DeclPrinter::printDeclType(QualType T, StringRef DeclName, bool Pack) {
-//   // Normally, a PackExpansionType is written as T[3]... (for instance, as a
-//   // template argument), but if it is the type of a declaration, the ellipsis
-//   // is placed before the name being declared.
-//   if (auto *PET = T->getAs<PackExpansionType>()) {
-//     Pack = true;
-//     T = PET->getPattern();
-//   }
-//   T.print(Out, Policy, (Pack ? "..." : "") + DeclName, Indentation);
-// }
-
-// void DeclPrinter::VisitVarDecl(VarDecl *D) {
-//   prettyPrintPragmas(D);
-//
-//   QualType T = D->getTypeSourceInfo()
-//     ? D->getTypeSourceInfo()->getType()
-//     : D->getASTContext().getUnqualifiedObjCPointerType(D->getType());
-//
-//  ...
-//  printDeclType(T, D->getName());
-// AD If class name added or changed also edit,
-// AD ../../../include/clang/StaticAnalyzer/Checkers/Checkers.td
-
-// void DeclPrinter::VisitParmVarDecl(ParmVarDecl *D) {
-//   VisitVarDecl(D);
-// }
