512b933df488f8b9faa7107430ed28ba8baa9b7c
adhuliya <lazynintel@gmail.com>
2019-01-12 20:37:25 +0530

    semi-automated-update

--- tests/demo-tests/test6.c
+++ tests/demo-tests/test6.c
@@ -1,10 +1,12 @@
 int main(int argc, char **argv) {
-  int x, y = 1;
+  int x, y = 1 + 2;
   int *u;
-  u = &x;
-
-  *u = y + 1 + 3;
-  return x;
+  if (y) {
+    u = &x;
+  } else {
+    *u = y + 1 + 3;
+  }
+  return x = func(1);
 }
 
 int func(int x) {
