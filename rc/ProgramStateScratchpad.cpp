f888a7f2f6dbd5daffd439d73b8f209e20f99be8
Ronak Chauhan <ronakchauhan97@gmail.com>
2019-01-12 20:18:30 +0530

    Some additions to ProgramStateScratchpad

--- rc/ProgramStateScratchpad.cpp
+++ rc/ProgramStateScratchpad.cpp
@@ -34,6 +34,23 @@ using namespace ento;
 // Rough struct for playing around with ProgramState
 class FunctionSignature {
 public:
+  typedef std::pair<std::string, std::string> Parameter;
+  typedef std::vector<Parameter> ParamVector;
+
+  bool operator==(const FunctionSignature &FS) const { return id == FS.id; }
+
+  void Profile(llvm::FoldingSetNodeID &ID) const { ID.AddInteger(1); }
+
+  FunctionSignature(std::string &func_name, std::string &ret_type,
+                    ParamVector &parameters) {
+    function_name = func_name;
+    return_type = ret_type;
+
+    for (auto it = parameters.begin(); it != parameters.end(); it++) {
+      param_list.push_back(*it);
+    }
+  }
+
   void log() {
     llvm::errs() << "Function name : " << function_name << "\n";
     llvm::errs() << "Return type : " << return_type << "\n";
@@ -54,10 +71,9 @@ public:
   }
 
 private:
+  int id;
   std::string function_name;
   std::string return_type;
-
-  typedef std::pair<std::string, std::string> Parameter;
   std::vector<Parameter> param_list;
 };
 
@@ -68,11 +84,13 @@ REGISTER_LIST_WITH_PROGRAMSTATE(SpanList, FunctionSignature)
 //===----------------------------------------------------------------------===//
 
 namespace {
-class MyCFGDumper : public Checker<check::ASTCodeBody> {
+class MyCFGDumper : public Checker<check::ASTCodeBody, check::EndAnalysis> {
 public:
   void checkASTCodeBody(const Decl *D, AnalysisManager &mgr,
                         BugReporter &BR) const {
     llvm::errs() << "----------------\n";
+
+    static int count = 1;
 
     PrintingPolicy Policy(mgr.getLangOpts());
     Policy.TerseOutput = false;
@@ -81,20 +99,20 @@ public:
 
     llvm::errs() << "--------\n";
 
-    // STEP 1.1: Print function name
-    llvm::errs() << "FuncName: ";
     const FunctionDecl *func_decl = dyn_cast<FunctionDecl>(D);
-    llvm::errs() << func_decl->getNameInfo().getAsString() << "\n";
+    std::string function_name = func_decl->getNameInfo().getAsString();
 
-    // STEP 1.2: Print function parameters.
-    std::string Proto;
-    llvm::errs() << "Params  : ";
+    typedef std::pair<std::string, std::string> Parameter;
+    typedef std::vector<Parameter> ParamVector;
+
+    // first element in pair is param name, second is it's type
+
+    ParamVector params;
     if (func_decl
             ->doesThisDeclarationHaveABody()) { //& !func_decl->hasPrototype())
                                                 //{
       for (unsigned i = 0, e = func_decl->getNumParams(); i != e; ++i) {
-        if (i)
-          Proto += ", ";
+
         const ParmVarDecl *paramVarDecl = func_decl->getParamDecl(i);
         const VarDecl *varDecl = dyn_cast<VarDecl>(paramVarDecl);
 
@@ -104,31 +122,35 @@ public:
                 ? varDecl->getTypeSourceInfo()->getType()
                 : varDecl->getASTContext().getUnqualifiedObjCPointerType(
                       varDecl->getType());
-        Proto += T.getAsString();
 
-        // Parameter name
-        Proto += " ";
-        Proto += varDecl->getNameAsString();
+        params.push_back(
+            std::make_pair(varDecl->getNameAsString(), T.getAsString()));
       }
     }
-    llvm::errs() << Proto << "\n";
 
-    // STEP 1.3: Print function return type.
     const QualType returnQType = func_decl->getReturnType();
-    llvm::errs() << "ReturnT : " << returnQType.getAsString() << "\n";
+    std::string return_type = returnQType.getAsString();
+
+    FunctionSignature FS(function_name, return_type, params);
+
+    // TODO: Somehow get access to current state
+    ProgramStateRef current_state;
+    const FunctionSignature &FSRef = FS;
+    FS.log();
+    ProgramStateRef new_state = current_state->add<SpanList>(FSRef);
 
     // STEP 2: Print the CFG.
-    if (CFG *cfg = mgr.getCFG(D)) {
-      for (auto bb : *cfg) {
-        unsigned bb_id = bb->getBlockID();
-        llvm::errs() << "BB" << bb_id << ":\n";
-        for (auto elem : *bb) {
-          // ref: https://clang.llvm.org/doxygen/CFG_8h_source.html#l00056
-          // ref for printing block:
-          // https://clang.llvm.org/doxygen/CFG_8cpp_source.html#l05234
-        }
-      }
-    }
+    // if (CFG *cfg = mgr.getCFG(D)) {
+    //   for (auto bb : *cfg) {
+    //     unsigned bb_id = bb->getBlockID();
+    //     llvm::errs() << "BB" << bb_id << ":\n";
+    //     for (auto elem : *bb) {
+    //       // ref: https://clang.llvm.org/doxygen/CFG_8h_source.html#l00056
+    //       // ref for printing block:
+    //       // https://clang.llvm.org/doxygen/CFG_8cpp_source.html#l05234
+    //     }
+    //   }
+    // }
   }
 };
 } // anonymous namespace
