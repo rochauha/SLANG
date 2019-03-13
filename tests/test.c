5b2b8ebe8924205c28dfb54dc1c53c36284e6fdd
Ronak Chauhan <ronakchauhan97@gmail.com>
2019-03-13 15:07:38 +0530

    Play around with function pointers

--- tests/test.c
+++ tests/test.c
@@ -1,14 +1,30 @@
-enum my_type {ANSHUMAN = 1, RONAK, RESHABH = 2};
-enum my_type g = ANSHUMAN;
-int x = 10, y = 20, *z = &x;
+// #include <stdio.h>
+
+int sum(int a, int b);
+// {
+// 	return a + b;
+// }
+
+int mul(int a, int b);
+// {
+// 	return a * b;
+// }
+
+typedef int INT;
+
+int a = 0;
+
 int main() {
+	// int b = 0;
+	INT (*fptr)(int, int);
+	fptr = sum;	
+	// a = 340;
+	// a = fptr(5, 45);
+	// printf("%d\n", a);
 
-	if(x++)
-		y++;
-	else 
-		x++;
+	// fptr = &mul;
+	// a = fptr(5, 45);
+	// printf("%d\n", a);
 
-	while (y)
-		(*z)++;
 
 }
