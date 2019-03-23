986c826632c888fb8bc14a1ae4a48999cf218b7e
adhuliya <lazynintel@gmail.com>
2019-03-23 20:36:43 +0530

    semi-automated-update

---
+++ ad/copyThere.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Copies the following files (as of now)
+# SlangCheckers/MyScratchpadChecker.cpp  #AD
+# SlangCheckers/SlangGenChecker.cpp  #AD
+# SlangCheckers/SlangBugReporterChecker.cpp  #AD
+# SlangCheckers/MyOwnChecker.cpp  #AD
+# SlangCheckers/MyTraverseAST.cpp  #AD
+# SlangCheckers/SlangTranslationUnit.cpp #AD
+# SlangCheckers/SlangExpr.cpp #AD
+# SlangCheckers/SlangUtil.cpp #AD
+
+cp -R SlangCheckers /home/codeman/itsoflife/local/packages-live/llvm-clang6/llvm/tools/clang/lib/StaticAnalyzer/Checkers/SlangCheckers
