8a632fbd510dfd9f87f5872a48043a679201e388
Ronak Chauhan <ronakchauhan97@gmail.com>
2019-04-03 07:05:24 +0530

    Enums are working (confirmed)

--- tests/test.c
+++ tests/test.c
@@ -4,12 +4,9 @@
 // test1.c: A sample test program. (This file)
 // test1.py: SPAN IR module for this file.
 
-int g;
 
-int main(int argc, char **argv) {
-    int x, y, z;
-    x = 10;
-    y = x + 10;
-    z = 10 / 400;
-    x = 30 && 50;
+enum my_type {ANSHUMAN, RONAK, RESHABH};
+
+int main() {
+    enum my_type x = ANSHUMAN + RONAK + RESHABH;
 }
